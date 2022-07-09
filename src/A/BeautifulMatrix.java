c08bd7d987276eca9464f7844ab7d55cea09c049
duhaammar <duha.abu99@gmail.com>
2022-07-09 19:31:33 +0000

    BeautifulMatrix main method

--- src/A/BeautifulMatrix.java
+++ src/A/BeautifulMatrix.java
@@ -2,9 +2,11 @@ package A;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BeautifulMatrix {
-    private ArrayList <ArrayList <Integer> > matrix = new ArrayList<ArrayList <Integer>>();
+    public static Scanner scanner = new Scanner(System.in);
+    private ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
 
     public ArrayList<ArrayList<Integer>> getMatrix() {
         return matrix;
@@ -14,16 +16,31 @@ public class BeautifulMatrix {
         this.matrix = matrix;
     }
 
-    public Integer numOfMoves(){
-        for( int i=0 ; i<matrix.size(); i++){
-            for (int j=0; j<matrix.get(i).size(); j++){
-                if(matrix.get(i).get(j).equals(1)){
-                    return Math.abs(2-i) +Math.abs(2-j);
+    public  Integer numOfMoves() {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix.get(i).size(); j++) {
+                if (matrix.get(i).get(j).equals(1)) {
+                    return Math.abs(2 - i) + Math.abs(2 - j);
                 }
             }
         }
         return 0;
     }
+
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> myMatrix = new ArrayList<ArrayList<Integer>>();
+        BeautifulMatrix matrix = new BeautifulMatrix();
+        for (int i = 0; i < 5; i++) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            for (int j = 0; j < 5; j++) {
+                arrayList.add(scanner.nextInt());
+            }
+            myMatrix.add(arrayList);
+        }
+        matrix.setMatrix(myMatrix);
+        int ans = matrix.numOfMoves();
+        System.out.println(ans);
+
+    }
 }
-
-
