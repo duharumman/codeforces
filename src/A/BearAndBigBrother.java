fa4f6b968d1a91497575ea3ccac0272284bec655
Duha Aburumman <59308955+Duharumman@users.noreply.github.com>
2022-07-09 14:41:17 +0000

    Update BearAndBigBrother.java
    update sol methods

--- src/A/BearAndBigBrother.java
+++ src/A/BearAndBigBrother.java
@@ -36,8 +36,8 @@ class  Calc{
     public Integer sol(int a , int b ){
         int years=0 ;
         while (a<=b){
-            a+=3;
-            b++;
+            a*=3;
+            b*=2;
             years++;
         }
         return years;
